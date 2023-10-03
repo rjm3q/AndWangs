ccdbaf9d0f01b7e3ba71f9966dc5ff1333ef1bab
rjm3q <robert.mabry86@gmail.com>
2023-10-03 10:25:10 -0500

    Add project files.

---
+++ DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AndWangs.Models;
+public class AndWangsDbContext : DbContext
+    {
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    public AndWangsDbContext(DbContextOptions<AndWangsDbContext>
+        {
+    }
+    
+}
